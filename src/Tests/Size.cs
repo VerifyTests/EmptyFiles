d09072505159e622839bee29b418edccdd934410
Simon Cropp <simon.cropp@gmail.com>
2023-12-23 21:16:14 +1100

    Update Size.cs

--- src/Tests/Size.cs
+++ src/Tests/Size.cs
@@ -1,11 +1,11 @@
 ﻿static class Size
 {
     static string[] SizeSuffixes =
-    {
+    [
         "bytes",
         "KB",
         "MB"
-    };
+    ];
 
     public static string Suffix(long value)
     {
